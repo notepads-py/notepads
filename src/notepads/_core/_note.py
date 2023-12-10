e8f569f5f4f265b6cc65c7c9dc5fa1d131884e48
notepads-py <153421252+notepads-py@users.noreply.github.com>
2023-12-10 03:30:17 -0500

    Create _note.py

---
+++ src/notepads/_core/_note.py
@@ -0,0 +1,42 @@
+class NotepadsNoteHandler(object):
+    def __init__(self):
+        self.notes = []
+        self.all = {}
+
+notepad: NotepadsNoteHandler = NotepadsNoteHandler()
+
+class NotepadsNote(object):
+    def __init__(self, name, content, *, author=None):
+        self.name: str = name
+        self.author: str = author
+        self.content: str = content
+        
+        notepad.notes.append(self)
+        notepad.all[self.name] = self
+
+    def __repr__(self):
+        return f'Note(author={self.author}, name={self.name}, content={self.content})'
+
+    def __str__(self):
+        return self.__repr__()
+
+    def read(self):
+        return self.content
+
+    def write(self, content):
+        self.content = content
+        return self
+
+    def wipe(self):
+        self.content = ''
+        return self
+
+    def delete(self):
+        try:
+            notepad.notes.remove(self)
+            del notepad.all[self.name]
+            return self
+        except:
+            return 'Unknown Note'
+
+    
