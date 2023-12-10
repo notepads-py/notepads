3afb79e0e06c2e0959013872cd96c1a5057c9785
notepads-py <153421252+notepads-py@users.noreply.github.com>
2023-12-10 03:32:15 -0500

    Create notepads_file_type.py

---
+++ src/notepads/_typing/notepads_file_type.py
@@ -0,0 +1,41 @@
+class File(object):
+    def __repr__():
+        return 'notepads.File[object]'
+    
+    def __class_getitem__(self, item, *args, **kwargs):
+        return item
+
+class FileAuthor(object):
+    def __repr__():
+        return 'notepads.FileAuthor[object]'
+
+    def __class_getitem__(self, item, *args, **kwargs):
+        return item
+
+class FileVersion(object):
+    def __repr__():
+        return 'notepads.FileVersion[object]'
+
+    def __class_getitem__(self, item, *args, **kwargs):
+        return item
+
+class FileDesc(object):
+    def __repr__():
+        return 'notepads.FileDesc[object]'
+
+    def __class_getitem__(self, item, *args, **kwargs):
+        return item
+
+class FileParent(object):
+    def __repr__():
+        return 'notepads.FileParent[object]'
+
+    def __class_getitem__(self, item, *args, **kwargs):
+        return item
+
+class FileContent(object):
+    def __repr__():
+        return 'notepads.FileContent[object]'
+
+    def __class_getitem__(self, item, *args, **kwargs):
+        return item
