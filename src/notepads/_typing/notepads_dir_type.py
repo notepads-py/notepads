25532dadcd7a84b864dbb2ca63ae4d6e85537bab
notepads-py <153421252+notepads-py@users.noreply.github.com>
2023-12-10 03:31:51 -0500

    Create notepads_dir_type.py

---
+++ src/notepads/_typing/notepads_dir_type.py
@@ -0,0 +1,10 @@
+class Directory(object):
+    def __repr__():
+        return 'notepads.Directory[object]'
+
+    def __class_getitem__(self, item, *args, **kwargs):
+        return item
+
+Dir: Directory = Directory
+Source: Directory = Directory
+Src: Directory = Directory
