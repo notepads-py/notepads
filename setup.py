415e46d2d7e08309370c2645f6bc2a9193ce6b7f
notepads-py <153421252+notepads-py@users.noreply.github.com>
2023-12-10 03:46:34 -0500

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,33 @@
+from setuptools import setup, find_packages
+import pathlib
+
+path_absolute: pathlib.Path = pathlib.Path(__file__).parent.absolute()
+
+setup(
+    name='notepads',
+    version='5.7.1',
+    description='Create runtime folders, files, and code notes. All stored in notepads environment.',
+    long_description=pathlib.Path(f"{path_absolute}/README.md").read_text(encoding="utf-8"),
+    long_description_content_type='text/markdown',
+    url='https://github.com/py-notepads',
+    author='notepads',
+    author_email='notepads.py@gmail.com',
+    license='MIT',
+    classifiers=[
+        'Development Status :: 1 - Planning',
+        'Development Status :: 2 - Pre-Alpha',
+        'Development Status :: 3 - Alpha',
+        'Development Status :: 4 - Beta',
+        'Development Status :: 5 - Production/Stable',
+        'Intended Audience :: Developers',
+        'Topic :: Software Development :: Build Tools',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.7',
+    ],
+    keywords='notes, notepad, notepads, np, nps',
+    install_requires=[
+    ],
+    include_package_data=True,
+    packages=find_packages(exclude=['tests', 'tests.*'])
+),
