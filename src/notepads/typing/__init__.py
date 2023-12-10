812ab96dbaef7737bcd300223b1d223e79eea278
notepads-py <153421252+notepads-py@users.noreply.github.com>
2023-12-10 03:34:27 -0500

    Create __init__.py

---
+++ src/notepads/typing/__init__.py
@@ -0,0 +1,31 @@
+from typing import (
+    Any,
+    Callable,
+    Dict,
+    List,
+    Optional,
+    Tuple,
+    Union,
+)
+
+from .._typing import (
+    Directory,
+    Dir,
+    Source,
+    Src,
+    File,
+    FileAuthor,
+    FileVersion,
+    FileDesc,
+    FileParent,
+    FileContent,
+    Folder,
+    FolderAuthor,
+    FolderVersion,
+    FolderDesc,
+    FolderFile,
+    Note,
+    NoteName,
+    NoteContent,
+    NoteAuthor
+)
