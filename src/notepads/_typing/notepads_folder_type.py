446e359ea7d5ef9d0b0a4002593462961bf4d834
notepads-py <153421252+notepads-py@users.noreply.github.com>
2023-12-10 03:32:39 -0500

    Create notepads_folder_type.py

---
+++ src/notepads/_typing/notepads_folder_type.py
@@ -0,0 +1,34 @@
+class Folder(object):
+    def __repr__():
+        return 'notepads.Folder[object]'
+
+    def __class_getitem__(self, item, *args, **kwargs):
+        return item
+
+class FolderAuthor(object):
+    def __repr__():
+        return 'notepads.FolderAuthor[object]'
+
+    def __class_getitem__(self, item, *args, **kwargs):
+        return item
+
+class FolderVersion(object):
+    def __repr__():
+        return 'notepads.FolderVersion[object]'
+
+    def __class_getitem__(self, item, *args, **kwargs):
+        return item
+
+class FolderDesc(object):
+    def __repr__():
+        return 'notepads.FolderDesc[object]'
+
+    def __class_getitem__(self, item, *args, **kwargs):
+        return item
+
+class FolderFile(object):
+    def __repr__():
+        return 'notepads.FolderFile[object]'
+
+    def __class_getitem__(self, item, *args, **kwargs):
+        return item
