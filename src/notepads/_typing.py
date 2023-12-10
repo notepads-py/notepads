ad04e70c54e6ea60d37ae33911e1fe993f05355d
notepads-py <153421252+notepads-py@users.noreply.github.com>
2023-12-10 03:30:54 -0500

    Create _typing.py

---
+++ src/notepads/_typing.py
@@ -0,0 +1,30 @@
+from .notepads_dir_type import (
+    Directory,
+    Dir,
+    Source,
+    Src,
+)
+
+from .notepads_file_type import (
+    File,
+    FileAuthor,
+    FileVersion,
+    FileDesc,
+    FileParent,
+    FileContent,
+)
+
+from .notepads_folder_type import (
+    Folder,
+    FolderAuthor,
+    FolderVersion,
+    FolderDesc,
+    FolderFile,
+)
+
+from .notepads_note_type import (
+    Note,
+    NoteName,
+    NoteContent,
+    NoteAuthor,
+)
