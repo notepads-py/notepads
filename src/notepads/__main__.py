95897f561b267b0793993b9b7c3f88063c1b574c
notepads-py <153421252+notepads-py@users.noreply.github.com>
2023-12-10 03:26:59 -0500

    Create __main__.py

---
+++ src/notepads/__main__.py
@@ -0,0 +1,34 @@
+from . import (
+    __version__,
+    __description__,
+    __python_version__,
+    __author__,
+    __license__,
+    __copyright__,
+    __credits__,
+    __maintainer__,
+    __email__,
+    __status__,
+    __repo__,
+)
+
+def main():
+    notepads_table: dict = {
+        'name': 'notepads',
+        'version': __version__,
+        'description': __description__,
+        'python_version': __python_version__,
+        'repo': __repo__,
+        'author': __author__,
+        'license': __license__,
+        'copyright': __copyright__,
+        'credits': __credits__,
+        'maintainer': __maintainer__,
+        'email': __email__,
+        'status': __status__,
+    }
+    for key, value in jsonr_table.items():
+        print(f'{key}: {value}')
+
+if __name__ == '__main__':
+    main()
