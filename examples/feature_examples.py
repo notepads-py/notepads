d69c0aa75111693d2106cdf276c974bd8009a4a2
notepads-py <153421252+notepads-py@users.noreply.github.com>
2023-12-10 06:01:28 -0500

    Create feature_examples.py

---
+++ examples/feature_examples.py
@@ -0,0 +1,97 @@
+import notepads
+
+notepads.Source \
+    .src='''  (Demo) Using notepads V5.7.5  '''
+
+
+'''
+Creating a file `test.txt`.
+'''
+
+notepads.file \
+    (name="test.txt")
+
+print(notepads.src)
+
+'''
+Output:
+
+Directory["/notepads/"]
+  🗎  test.txt
+'''
+
+'''
+Creating a folder `examples`.
+'''
+
+notepads.folder \
+    (name="examples")
+
+print(notepads.src)
+
+'''
+Output:
+
+Directory["/notepads/"]
+  🗀  examples
+  🗎  test.txt
+'''
+
+'''
+Move `test.txt` to `examples`.
+'''
+
+notepads.get('test.txt').move('examples')
+
+print(notepads.src)
+
+'''
+Output:
+
+Directory["/notepads/"]
+  🗀  examples
+   ↳ 🗎  test.txt
+'''
+
+'''
+Create a file `main.py`.
+'''
+
+notepads.file \
+    (name="main.py")
+
+print(notepads.src)
+
+'''
+Output:
+
+Directory["/notepads/"]
+  🗀  examples
+   ↳ 🗎  test.txt
+  🗎  main.py
+'''
+
+'''
+Write simple program to file `main.py`
+'''
+
+main_py = notepads.get('main.py')
+
+main_py.write \
+('''
+print("Hello World!")
+
+for i in range(10):
+    print(i)
+''')
+
+print(main_py.read())
+
+'''
+Output:
+
+print("Hello World!")
+
+for i in range(10):
+    print(i)
+'''
